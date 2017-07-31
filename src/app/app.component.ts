d505356f5f6ad14c55f52087e2cbcc2fadc0ef58
Noor <programmer26@gmail.com>
2017-07-31 00:49:00 -0700

    AngularJS2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
 import { Component } from '@angular/core';
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+
+    trigger('listAnimation', [
+      transition('* => *', [
+
+        query(':enter', style({ opacity: 0 }), {optional: true}),
+
+        query(':enter', stagger('300ms', [
+          animate('1s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+          ]))
+        ]), {optional: true}),
+
+
+          query(':leave', stagger('300ms', [
+          animate('1s ease-in', keyframes([
+            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 0, transform: 'translateY(-75%)',     offset: 1.0}),
+          ]))]), {optional: true})
+      ])
+    ]),
+     trigger('explainerAnim', [
+      transition('* => *', [
+        query('.col', style({ opacity: 0, transform: 'translateX(-40px)' })),
+
+        query('.col', stagger('500ms', [
+          animate('800ms 1.2s ease-out', style({ opacity: 1, transform: 'translateX(0)' })),
+        ])),
+
+        query('.col', [
+          animate(1000, style('*'))
+        ])
+      ])
+    ])
+
+  ]
 })
 export class AppComponent {
-  title = 'app';
+  items = [];
+
+  constructor() {
+    this.items = ['Hey this is an item', 'Here is another one', 'This is awesome'];
+  }
+
+  pushItem() {
+    this.items.push('Oh yeah that is awesome');
+  }
+  removeItem() {
+    this.items.pop();
+  }
 }
+
